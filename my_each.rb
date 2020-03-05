37156f1cfa49335b4194cb19cab16fe3cc461845
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-05 22:15:41 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,24 @@
-def my_each # put argument(s) here
-  # code here
+
+collection = [1, 2, 3, 4]
+
+def my_each(array)
+  if block_given?
+    i = 0
+ 
+    while i < array.length do
+      yield(array[i])
+      i = i + 1
+      puts i
+      
+    end
+ 
+    array
+  else
+    puts "Hey! No block was given!"
+  end
 end
+
+my_each(collection) do |i|
+  puts i 
+end
+
